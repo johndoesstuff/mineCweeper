79eaccf26697f03265a5dbea71ada9b7e5c42fc6
johndoesstuff <johndoesstuff03@gmail.com>
2024-12-16 01:16:02 -0500

    cursor movement

--- src/main.c
+++ src/main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <ncurses.h>
 #include <stdlib.h>
+#include <time.h>
 
 int width, height;
 int center_x, center_y;
+int game_over;
 
 typedef struct {
 	int mine;
@@ -31,7 +33,7 @@ Cell** initializeBoard(int width, int height) {
 
 	for (int y = 0; y < height; y++) {
 		for (int x = 0; x < width; x++) {
-			board[y][x].mine = 0;
+			board[y][x].mine = rand() % 10 == 1; // 1/10 mine chance
 			board[y][x].count = 0;
 			board[y][x].revealed = 0;
 		}
@@ -42,6 +44,7 @@ Cell** initializeBoard(int width, int height) {
 
 int main(int argc, char *argv[]) {
 
+	srand(time(NULL));
 	if (argc != 3) {
 		fprintf(stderr, "Usage: %s <width> <height>\n", argv[0]);
 		return 1;
@@ -49,6 +52,8 @@ int main(int argc, char *argv[]) {
 
 	width = atoi(argv[1]);
 	height = atoi(argv[2]);
+
+	int cursor_x, cursor_y;
 
 	if (width < 8 || height < 8) {
 		fprintf(stderr, "Width and height must be at least 8.\n");
@@ -83,12 +88,35 @@ int main(int argc, char *argv[]) {
 	center_x = max_x/2;
 	center_y = max_y/2;
 
-	drawBoard(board);
+	cursor_x = width/2;
+	cursor_y = height/2;
 
-	refresh();
+	game_over = 0;
+	wchar_t ch;
 
-	getch();
+	while (!game_over) {
+		erase();
+		drawBoard(board);
+		mvprintw(cursor_y + center_y - height/2, cursor_x*3 + center_x - 3*width/2, ">*<");
+		refresh();
 
+		ch = getch();
+
+		switch (ch) {
+			case KEY_UP:
+				if (cursor_y > 0) cursor_y--;
+				break;
+			case KEY_DOWN:
+				if (cursor_y < height - 1) cursor_y++;
+				break;
+			case KEY_LEFT:
+				if (cursor_x > 0) cursor_x--;
+				break;
+			case KEY_RIGHT:
+				if (cursor_x < width - 1) cursor_x++;
+				break;
+		}
+	}
 	endwin();
 	return 0;
 }
