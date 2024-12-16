17d1670c910810dc46fdb16660f960328486f910
johndoesstuff <johndoesstuff03@gmail.com>
2024-12-16 00:27:14 -0500

    initial

--- makefile
+++ makefile
@@ -5,13 +5,19 @@ SRCDIR = src
 BUILDDIR = build
 
 SRC = $(SRCDIR)/main.c
+OBJ = $(SRC:.c=.o)
 TARGET = $(BUILDDIR)/mineCweeper
+
+LDFLAGS = -lncurses
 
 all: $(TARGET)
 
 $(TARGET): $(SRC)
 	mkdir -p $(BUILDDIR)
-	$(CC) $(CFLAGS) $< -o $@
+	$(CC) $(CFLAGS) -o $@ $^ $(LDFLAGS)
+
+%.o: %.c
+	$(CC) $(CFLAGS) -c $< -o $@
 
 clean:
 	rm -rf $(BUILDDIR)
--- src/main.c
+++ src/main.c
@@ -1,5 +1,45 @@
 #include <stdio.h>
+#include <ncurses.h>
+#include <stdlib.h>
 
-int main() {
+int main(int argc, char *argv[]) {
+
+	int width, height;
+	if (argc != 3) {
+		fprintf(stderr, "Usage: %s <width> <height>\n", argv[0]);
+		return 1;
+	}
+
+	width = atoi(argv[1]);
+	height = atoi(argv[2]);
+
+	if (width <= 8 || height <= 8) {
+		fprintf(stderr, "Width and height must be at least 8.\n");
+		return 1;
+	}
 	
+	initscr();
+	cbreak();
+	noecho();
+	keypad(stdscr, TRUE);
+	curs_set(0);
+
+	int max_y, max_x;
+	getmaxyx(stdscr, max_y, max_x);
+
+	if (width*3 >= max_x) {
+		fprintf(stderr, "Too wide of a board for current screen size");
+	}
+
+	if (height >= max_y) {
+		fprintf(stderr, "Too tall of a board for current screen size");
+	}
+
+	mvprintw(0, 0, "MineCweeper");
+	refresh();
+
+	getch();
+
+	endwin();
+	return 0;
 }
